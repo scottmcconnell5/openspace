3063ad777bbbd0cb064817029c2ad57f10aa0434
Scott McConnell <skm44@duke.edu>
2018-10-13 11:12:59 -0400

    More modulated starting structure

--- myapp/client/main.js
+++ myapp/client/main.js
@@ -1,33 +1,4 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// Client entry point, imports all client code
 
-import './main.html';
-
-Hosts = new Mongo.Collection('hosts');
-
-Template.hostTemplate.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hostTemplate.helpers({
-	hosts: function() {
-		return Hosts.find();
-	},
-	counter() {
-		return Template.instance().counter.get();
-	},
-});
-
-Template.hostTemplate.events({
-	'click #HostInfoSubmit': function(event, instance) {
-		debugger;
-		console.log("Clicked");
-		var firstName = event.target.title.value;
-		Hosts.insert({
-			firstName: firstName,
-			createdAt: new Date()
-		});
-		return false;
-	},
-});
+import '/imports/startup/client';
+import '/imports/startup/both';
